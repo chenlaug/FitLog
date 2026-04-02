9bb73aa07cf8b1f08229dbe690b7d40676554326
chenlaug <ch.laughan@gmail.com>
2026-04-02 19:57:03 +0200

    refactor(UserRestController): update return type and output mapping

--- Back-end/src/main/java/com/example/FitLog/user/controller/UserRestController.java
+++ Back-end/src/main/java/com/example/FitLog/user/controller/UserRestController.java
@@ -1,7 +1,10 @@
-package com.example.FitLog.user.Contoller;
+package com.example.FitLog.user.controller;
 
 import com.example.FitLog.user.DTO.UserDTO;
+import com.example.FitLog.user.mapper.UserMapper;
+import com.example.FitLog.user.model.UserEntity;
 import com.example.FitLog.user.model.exception.UserCreationException;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import com.example.FitLog.user.service.UserService;
@@ -13,14 +16,11 @@ public class UserRestController {
         this.userService = userService;
     }
 
-    @GetMapping("user/hello")
-    public String hello() {
-        return userService.sayHello();
-    }
-
     @PostMapping("user/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public String create(@RequestBody UserDTO.PostInput input) throws UserCreationException {
-        return userService.createUser(input.getName());
+    public String create(@Valid @RequestBody UserDTO.PostInput input) throws UserCreationException {
+        UserEntity newUser = userService.createUser(input.getName(),input.getEmail(),input.getPassword());
+        UserDTO.PostOutput output = UserMapper.toPostOutput(newUser);
+        return output.toString();
     }
 }
