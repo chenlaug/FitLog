df52cdfcc2e1fc59eb07c94f882ee862ba85d26e
chenlaug <ch.laughan@gmail.com>
2026-04-02 19:25:32 +0200

    feat(Exception)Add custom error for validation

--- Back-end/src/main/java/com/example/FitLog/Configuration/GlobalExceptionableHandler.java
+++ Back-end/src/main/java/com/example/FitLog/Configuration/GlobalExceptionableHandler.java
@@ -5,8 +5,12 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionableHandler {
@@ -27,5 +31,13 @@ public class GlobalExceptionableHandler {
     public ResponseEntity<ProblemDetail> cathUserCreation(UserCreationException ex) {
         ProblemDetail pm = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(pm);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
     }
 }
