2f8c95808aec96126b1f0db161bfcb35a8af776b
chenlaug <ch.laughan@gmail.com>
2026-04-03 01:43:42 +0200

    feat(migration): add constraints to app_user table for email and password

---
+++ Back-end/src/main/resources/db/migration/V5__add_constraints_to_app_user.sql
@@ -0,0 +1,3 @@
+ALTER TABLE app_user ALTER COLUMN email SET NOT NULL;
+ALTER TABLE app_user ADD CONSTRAINT uq_app_user_email UNIQUE (email);
+ALTER TABLE app_user ALTER COLUMN password SET NOT NULL;
