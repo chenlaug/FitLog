ebe918acd60e1e7cdce8abd50bea6acbb799ffe6
chenlaug <ch.laughan@gmail.com>
2026-04-10 04:06:43 +0200

    feat(user): add updateUser endpoint to modify user details

--- Back-end/src/main/java/com/example/FitLog/user/controller/UserRestController.java
+++ Back-end/src/main/java/com/example/FitLog/user/controller/UserRestController.java
@@ -35,4 +35,9 @@ public class UserRestController {
     public ResponseEntity<UserDTO.PathOutput> updateUser(@Valid @RequestBody UserDTO.PatchInput input) {
         return userService.updateById(input.getName(), input.getEmail());
     }
+
+    @PatchMapping("")
+    public ResponseEntity<UserDTO.PathOutput> updateUser(@Valid @RequestBody UserDTO.PatchInput input) {
+        return userService.updateById(input.getName(), input.getEmail());
+    }
 }
