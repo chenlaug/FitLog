52f850c3f6cd18f6b922eb6b18b9a7d3b56ccf62
chenlaug <ch.laughan@gmail.com>
2026-04-15 04:12:35 +0200

    feat(exception): add ExerciseException for handling exercise-related errors

---
+++ Back-end/src/main/java/com/example/FitLog/exercise/model/exception/ExerciseException.java
@@ -0,0 +1,28 @@
+package com.example.FitLog.exercise.model.exception;
+
+import com.example.FitLog.user.model.exception.UserException;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ExerciseException extends RuntimeException {
+    private final HttpStatus status;
+    public ExerciseException(HttpStatus status,String message) {
+        super(message);
+        this.status = status;
+    }
+    public static ExerciseException notFound() {
+        return new ExerciseException(HttpStatus.NOT_FOUND, "Exercise not found");
+    }
+
+    public static ExerciseException alreadyExists() {
+        return new ExerciseException(HttpStatus.CONFLICT, "Exercise already exists");
+    }
+
+    public static ExerciseException creationFailed(String reason) {
+        return new ExerciseException(HttpStatus.BAD_REQUEST, "Exercise creation failed: " + reason);
+    }
+    public static ExerciseException updateFailed(String reason) {
+        return new ExerciseException(HttpStatus.BAD_REQUEST, "Exercise update failed: " + reason);
+    }
+}
